f5d5beea375d624adc1d572888b8dcde673f4da5
artathred <arta@thredapps.com>
2022-10-09 05:07:06 -0400

    REINIT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,35 @@
-import { ChangeDetectorRef, Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import {
+  ChangeDetectorRef,
+  Component,
+  Inject,
+  PLATFORM_ID,
+} from '@angular/core';
 import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'frontend';
 
-  openMobileMenu = false
-  expandedSearch = false
+  openMobileMenu = false;
+  expandedSearch = false;
 
-  constructor(private cdr: ChangeDetectorRef){
-    AOS.init()
+  constructor(
+    private cdr: ChangeDetectorRef,
+    @Inject(PLATFORM_ID) private platformID: Object
+  ) {
+    if (isPlatformBrowser(this.platformID)) {
+      AOS.init();
+    }
   }
 
-  updateBar(event: boolean){
-    console.log(event)
+  updateBar(event: boolean) {
+    console.log(event);
     this.expandedSearch = event;
-    this.cdr.detectChanges()
+    this.cdr.detectChanges();
   }
 }
