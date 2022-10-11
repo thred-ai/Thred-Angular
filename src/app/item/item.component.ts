1c47eae9cb5fc30512869abdf55f4f416e4c8aa5
artathred <arta@thredapps.com>
2022-10-10 22:34:46 -0400

    zoom

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from '../category.model';
 import { Chain } from '../chain.model';
 import { Signature } from '../signature.model';
 import { Util } from '../util.model';
@@ -31,6 +32,134 @@ export class ItemComponent implements OnInit {
     0,
     [new Chain('Polygon', 137)],
     'https://firebasestorage.googleapis.com/v0/b/clothingapp-ed125.appspot.com/o/Resources%2Fimg.jpeg?alt=media'
+  )
+
+  category = new Category(
+    'Discover More',
+    1,
+    [
+      new Util(
+        '123',
+        '123',
+        new Signature('My New Util', '123', '123', 3, 100, '123'),
+        'Thred Developer',
+        'My New Util',
+        [
+          'https://firebasestorage.googleapis.com/v0/b/clothingapp-ed125.appspot.com/o/Resources%2Fthumbnail_image%20(69).png?alt=media',
+        ],
+        '123',
+        'My New Util for my New NFT Collection.',
+        100,
+        3,
+        true,
+        false,
+        0,
+        0,
+        [new Chain('Polygon', 137)],
+        'https://firebasestorage.googleapis.com/v0/b/clothingapp-ed125.appspot.com/o/Resources%2Fimg.jpeg?alt=media'
+      ),
+      new Util(
+        '123',
+        '123',
+        new Signature('My New Util', '123', '123', 3, 100, '123'),
+        'Thred Developer',
+        'My New Util',
+        [
+          'https://firebasestorage.googleapis.com/v0/b/clothingapp-ed125.appspot.com/o/Resources%2Fthumbnail_image%20(69).png?alt=media',
+        ],
+        '123',
+        'My New Util for my New NFT Collection.',
+        100,
+        3,
+        true,
+        false,
+        0,
+        0,
+        [new Chain('Polygon', 137)],
+        'https://firebasestorage.googleapis.com/v0/b/clothingapp-ed125.appspot.com/o/Resources%2Fimg.jpeg?alt=media'
+      ),
+      new Util(
+        '123',
+        '123',
+        new Signature('My New Util', '123', '123', 3, 100, '123'),
+        'Thred Developer',
+        'My New Util',
+        [
+          'https://firebasestorage.googleapis.com/v0/b/clothingapp-ed125.appspot.com/o/Resources%2Fthumbnail_image%20(69).png?alt=media',
+        ],
+        '123',
+        'My New Util for my New NFT Collection.',
+        100,
+        3,
+        true,
+        false,
+        0,
+        0,
+        [new Chain('Polygon', 137)],
+        'https://firebasestorage.googleapis.com/v0/b/clothingapp-ed125.appspot.com/o/Resources%2Fimg.jpeg?alt=media'
+      ),
+      new Util(
+        '123',
+        '123',
+        new Signature('My New Util', '123', '123', 3, 100, '123'),
+        'Thred Developer',
+        'My New Util',
+        [
+          'https://firebasestorage.googleapis.com/v0/b/clothingapp-ed125.appspot.com/o/Resources%2Fthumbnail_image%20(69).png?alt=media',
+        ],
+        '123',
+        'My New Util for my New NFT Collection.',
+        100,
+        3,
+        true,
+        false,
+        0,
+        0,
+        [new Chain('Polygon', 137)],
+        'https://firebasestorage.googleapis.com/v0/b/clothingapp-ed125.appspot.com/o/Resources%2Fimg.jpeg?alt=media'
+      ),
+      new Util(
+        '123',
+        '123',
+        new Signature('My New Util', '123', '123', 3, 100, '123'),
+        'Thred Developer',
+        'My New Util',
+        [
+          'https://firebasestorage.googleapis.com/v0/b/clothingapp-ed125.appspot.com/o/Resources%2Fthumbnail_image%20(69).png?alt=media',
+        ],
+        '123',
+        'My New Util for my New NFT Collection.',
+        100,
+        3,
+        true,
+        false,
+        0,
+        0,
+        [new Chain('Polygon', 137)],
+        'https://firebasestorage.googleapis.com/v0/b/clothingapp-ed125.appspot.com/o/Resources%2Fimg.jpeg?alt=media'
+      ),
+      new Util(
+        '123',
+        '123',
+        new Signature('My New Util', '123', '123', 3, 100, '123'),
+        'Thred Developer',
+        'My New Util',
+        [
+          'https://firebasestorage.googleapis.com/v0/b/clothingapp-ed125.appspot.com/o/Resources%2Fthumbnail_image%20(69).png?alt=media',
+        ],
+        '123',
+        'My New Util for my New NFT Collection.',
+        100,
+        3,
+        true,
+        false,
+        0,
+        0,
+        [new Chain('Polygon', 137)],
+        'https://firebasestorage.googleapis.com/v0/b/clothingapp-ed125.appspot.com/o/Resources%2Fimg.jpeg?alt=media'
+      ),
+    ],
+    4
   )
 
   ngOnInit(): void {
