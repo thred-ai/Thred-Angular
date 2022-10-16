e7e78e5ad9896d85e0d74cb418b7f9f1f5ef5bba
artathred <arta@thredapps.com>
2022-10-16 16:16:26 -0400

    push

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, Input, OnInit, PLATFORM_ID } from '@angular/core';
 import { ethers } from 'ethers';
 import { Category } from '../category.model';
 import { Chain } from '../chain.model';
@@ -9,10 +10,9 @@ import { Util } from '../util.model';
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.scss']
+  styleUrls: ['./category.component.scss'],
 })
 export class CategoryComponent implements OnInit {
-
   @Input() category?: Category = new Category(
     'Discover More',
     '1',
@@ -213,13 +213,22 @@ export class CategoryComponent implements OnInit {
     4
   );
 
-  openItem(id: string){
-    this.loadService.openItem(id)
-  }
-  
-  constructor(private loadService: LoadService) { }
-
-  ngOnInit(): void {
+  openItem(id: string) {
+    this.loadService.openItem(id);
   }
 
+  scrollToRow() {
+    if (isPlatformBrowser(this.platformID)) {
+      const el = document.querySelector('.items-row');
+      el?.scrollIntoView({ block: 'start', behavior: 'smooth' });
+
+    }
+  }
+
+  constructor(
+    private loadService: LoadService,
+    @Inject(PLATFORM_ID) private platformID: Object
+  ) {}
+
+  ngOnInit(): void {}
 }
