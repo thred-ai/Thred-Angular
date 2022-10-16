5fc5751559324d47021a3be5b44558f40e5fae03
artathred <arta@thredapps.com>
2022-10-16 15:38:48 -0400

    YAY

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
+import { CategoryComponent } from './category/category.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
 import { ItemComponent } from './item/item.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent, pathMatch: 'full' },
   { path: 'utils/:util', component: ItemComponent, pathMatch: 'full' },
+  { path: 'groups/:group', component: CategoryComponent, pathMatch: 'full' },
   { path: 'account', component: AuthComponent, pathMatch: 'full' },
   {
     path: 'dashboard/:user',
     component: DashboardComponent,
     pathMatch: 'full',
   },
+  { path: 'home', component: HomeComponent, pathMatch: 'full' },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: ':any', redirectTo: '/home', pathMatch: 'full' },
 ];
--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ethers } from 'ethers';
+import { Category } from '../category.model';
+import { Chain } from '../chain.model';
+import { LoadService } from '../load.service';
+import { Signature } from '../signature.model';
+import { Util } from '../util.model';
 
 @Component({
   selector: 'app-category',
@@ -7,7 +13,211 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryComponent implements OnInit {
 
-  constructor() { }
+  @Input() category?: Category = new Category(
+    'Discover More',
+    '1',
+    [
+      new Util(
+        '123',
+        '123',
+        new Signature(
+          'My New Util',
+          '123',
+          '123',
+          3,
+          100,
+          1665828483000,
+          1665828483000,
+          '123'
+        ),
+        1665828483000,
+        1665828483000,
+        'Thred Developer',
+        'My New Util',
+        [
+          'https://storage.googleapis.com/thred-protocol.appspot.com/Test/app_img.png',
+        ],
+        '123',
+        'My New Util for my New NFT Collection.',
+        100,
+        ethers.utils.parseEther('100'),
+        3,
+        true,
+        false,
+        0,
+        0,
+        [new Chain('Polygon', 137, 'MATIC')],
+        'https://storage.googleapis.com/thred-protocol.appspot.com/Test/img.png'
+      ),
+      new Util(
+        '123',
+        '123',
+        new Signature(
+          'My New Util',
+          '123',
+          '123',
+          3,
+          100,
+          1665828483000,
+          1665828483000,
+          '123'
+        ),
+        1665828483000,
+        1665828483000,
+        'Thred Developer',
+        'My New Util',
+        [
+          'https://storage.googleapis.com/thred-protocol.appspot.com/Test/app_img.png',
+        ],
+        '123',
+        'My New Util for my New NFT Collection.',
+        100,
+        ethers.utils.parseEther('100'),
+        3,
+        true,
+        false,
+        0,
+        0,
+        [new Chain('Polygon', 137, 'MATIC')],
+        'https://storage.googleapis.com/thred-protocol.appspot.com/Test/img.png'
+      ),
+      new Util(
+        '123',
+        '123',
+        new Signature(
+          'My New Util',
+          '123',
+          '123',
+          3,
+          100,
+          1665828483000,
+          1665828483000,
+          '123'
+        ),
+        1665828483000,
+        1665828483000,
+        'Thred Developer',
+        'My New Util',
+        [
+          'https://storage.googleapis.com/thred-protocol.appspot.com/Test/app_img.png',
+        ],
+        '123',
+        'My New Util for my New NFT Collection.',
+        100,
+        ethers.utils.parseEther('100'),
+        3,
+        true,
+        false,
+        0,
+        0,
+        [new Chain('Polygon', 137, 'MATIC')],
+        'https://storage.googleapis.com/thred-protocol.appspot.com/Test/img.png'
+      ),
+      new Util(
+        '123',
+        '123',
+        new Signature(
+          'My New Util',
+          '123',
+          '123',
+          3,
+          100,
+          1665828483000,
+          1665828483000,
+          '123'
+        ),
+        1665828483000,
+        1665828483000,
+        'Thred Developer',
+        'My New Util',
+        [
+          'https://storage.googleapis.com/thred-protocol.appspot.com/Test/app_img.png',
+        ],
+        '123',
+        'My New Util for my New NFT Collection.',
+        100,
+        ethers.utils.parseEther('100'),
+        3,
+        true,
+        false,
+        0,
+        0,
+        [new Chain('Polygon', 137, 'MATIC')],
+        'https://storage.googleapis.com/thred-protocol.appspot.com/Test/img.png'
+      ),
+      new Util(
+        '123',
+        '123',
+        new Signature(
+          'My New Util',
+          '123',
+          '123',
+          3,
+          100,
+          1665828483000,
+          1665828483000,
+          '123'
+        ),
+        1665828483000,
+        1665828483000,
+        'Thred Developer',
+        'My New Util',
+        [
+          'https://storage.googleapis.com/thred-protocol.appspot.com/Test/app_img.png',
+        ],
+        '123',
+        'My New Util for my New NFT Collection.',
+        100,
+        ethers.utils.parseEther('100'),
+        3,
+        true,
+        false,
+        0,
+        0,
+        [new Chain('Polygon', 137, 'MATIC')],
+        'https://storage.googleapis.com/thred-protocol.appspot.com/Test/img.png'
+      ),
+      new Util(
+        '123',
+        '123',
+        new Signature(
+          'My New Util',
+          '123',
+          '123',
+          3,
+          100,
+          1665828483000,
+          1665828483000,
+          '123'
+        ),
+        1665828483000,
+        1665828483000,
+        'Thred Developer',
+        'My New Util',
+        [
+          'https://storage.googleapis.com/thred-protocol.appspot.com/Test/app_img.png',
+        ],
+        '123',
+        'My New Util for my New NFT Collection.',
+        100,
+        ethers.utils.parseEther('100'),
+        3,
+        true,
+        false,
+        0,
+        0,
+        [new Chain('Polygon', 137, 'MATIC')],
+        'https://storage.googleapis.com/thred-protocol.appspot.com/Test/img.png'
+      ),
+    ],
+    4
+  );
+
+  openItem(id: string){
+    this.loadService.openItem(id)
+  }
+  
+  constructor(private loadService: LoadService) { }
 
   ngOnInit(): void {
   }
