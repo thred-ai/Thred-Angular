7912525212d70c0c92692f06b7f487c7ce386c41
artathred <arta@thredapps.com>
2022-11-04 01:31:22 -0400

    s

--- src/app/app-frame/app-frame.component.ts
+++ src/app/app-frame/app-frame.component.ts
@@ -29,46 +29,46 @@ export class AppFrameComponent implements OnInit {
 
     //Unique session key for the active app instance
 
-    protocolSDK.setAppListener(data.app.id ?? '1', async (data) => {
-      //Contract Details
-      const contractData = data.contract;
-      const address = contractData.address;
-      const abi = contractData.abi;
+    // protocolSDK.setAppListener(data.app.id ?? '1', async (data) => {
+    //   //Contract Details
+    //   const contractData = data.contract;
+    //   const address = contractData.address;
+    //   const abi = contractData.abi;
 
-      //Function Details
-      const name = data.name;
-      const params = data.params;
-      const type = data.type ?? 'transact';
+    //   //Function Details
+    //   const name = data.name;
+    //   const params = data.params;
+    //   const type = data.type ?? 'transact';
 
-      //Get Connected Signer
-      let signer = this.provider?.getSigner(); //retrieve signer
+    //   //Get Connected Signer
+    //   let signer = this.provider?.getSigner(); //retrieve signer
 
-      let contract = new ethers.Contract(address, abi, signer);
+    //   let contract = new ethers.Contract(address, abi, signer);
 
-      if (type == 'transact') {
-        const value = data.value;
-        const waitMode = data.waitMode;
+    //   if (type == 'transact') {
+    //     const value = data.value;
+    //     const waitMode = data.waitMode;
 
-        //Use ethers.js to call the function on the contract with the current provider.
-        let transaction = await contract[name](...params, {
-          value,
-        });
+    //     //Use ethers.js to call the function on the contract with the current provider.
+    //     let transaction = await contract[name](...params, {
+    //       value,
+    //     });
 
-        //Check wait mode passed by the app
-        if (waitMode == 'wait' || waitMode == 'wait_update') {
-          if (waitMode == 'wait_update') {
-            protocolSDK.sendTransactionReceipt(transaction);
-          }
-          await transaction.wait();
-        }
+    //     //Check wait mode passed by the app
+    //     if (waitMode == 'wait' || waitMode == 'wait_update') {
+    //       if (waitMode == 'wait_update') {
+    //         protocolSDK.sendTransactionReceipt(transaction);
+    //       }
+    //       await transaction.wait();
+    //     }
 
-        protocolSDK.sendTransactionReceipt(transaction);
-      } else if (type == 'view') {
-        let data = await contract[name](...params);
+    //     protocolSDK.sendTransactionReceipt(transaction);
+    //   } else if (type == 'view') {
+    //     let data = await contract[name](...params);
 
-        protocolSDK.sendViewData(data);
-      }
-    });
+    //     protocolSDK.sendViewData(data);
+    //   }
+    // });
   }
 
   ngOnInit(): void {}
